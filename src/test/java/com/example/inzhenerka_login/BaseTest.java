c413bba3a246032cf29de9d574dab245b06cf2bd
diaskrv <kurmashevd@inbox.ru>
2026-02-27 20:08:20 +0500

    Update Base test: Adding @AfterEach test for logging out

--- src/test/java/com/example/inzhenerka_login/BaseTest.java
+++ src/test/java/com/example/inzhenerka_login/BaseTest.java
@@ -3,6 +3,7 @@ package com.example.inzhenerka_login;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -11,5 +12,11 @@ public class BaseTest {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
         // Configuration.holdBrowserOpen = true; // Полезно для отладки
+    }
+
+    @AfterEach
+    public void tearDownTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.logout();
     }
 }
