02f9f73b64ed02287927fb6b0cb5f3622e6cf414
diaskrv <kurmashevd@inbox.ru>
2026-04-22 19:12:49 +0500

    Upgrade localization ticketon test

--- src/test/java/com/example/inzhenerka_login/TicketonTestFields.java
+++ src/test/java/com/example/inzhenerka_login/TicketonTestFields.java
@@ -1,2 +1,87 @@
-package com.example.inzhenerka_login;public class TicketonTestFields {
+package com.example.inzhenerka_login;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
+
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+
+public class TicketonTestFields {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://ticketon.kz";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void closeBanner() {
+        open("/");
+        var closeButton = $("[class^='MainBannerModal_closeIcon']");
+
+        if (closeButton.is(Condition.visible)) {
+            closeButton.click();
+            closeButton.shouldBe(Condition.hidden);
+        }
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @MethodSource("languageProvider")
+    void testHtmlLangAttribute(String btnText, String expectedLang, String expectedText) {
+        var langList = $("[class^='Languages_wrapperLanguagesBtn']");
+        langList.click();
+
+        $("[class^='DropdownList_scrollWrapper']").shouldBe(Condition.visible);
+        $(byText(btnText)).click();
+        $("[class^='DropdownList_scrollWrapper']").shouldBe(Condition.hidden);
+
+        $("html").shouldHave(Condition.attribute("lang", expectedLang));
+        $("body").shouldHave(Condition.text(expectedText));
+    }
+
+    //У сайта Ticketon значение lang в атрибуте html никак не меняется и остается как ru, что не совсем является корректным
+    static Stream<Arguments> languageProvider() {
+        return Stream.of(
+                Arguments.of("Eng", "ru", "Event"),
+                Arguments.of("Рус", "ru", "событий"),
+                Arguments.of("Қаз", "ru", "Оқиғалар")
+        );
+    }
+
+    @Test
+    void testCitySelectionFilter() {
+        open("/");
+
+        $("[data-testid='city-selector-trigger']").click();
+
+        $(By.linkText("Алматы")).click();
+
+        $("[data-testid='city-selector-trigger']").shouldHave(Condition.text("Алматы"));
+    }
+
+    @Disabled
+    @Test
+    void testSearchFieldClear() {
+        open("/");
+
+        var searchInput = $(By.xpath("//input[@placeholder='Поиск событий']"));
+
+        searchInput.setValue("Saluki");
+        searchInput.shouldHave(Condition.value("Saluki"));
+
+        searchInput.clear();
+
+        searchInput.shouldBe(Condition.empty);
+    }
 }
